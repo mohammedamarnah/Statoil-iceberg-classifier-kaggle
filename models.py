c2392eeef325fc32313001fc915ca67911e9dda6
mohammedamarnah <m.amarnah@gmail.com>
2018-01-10 19:19:18 +0200

    0.1478 score, CNN + Data Augmentation + Min-Max Stacking

--- models.py
+++ models.py
@@ -1,28 +1,54 @@
-from keras.models import Sequential 
-from keras.layers import Convolution2D, GlobalAveragePooling2D, Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Lambda
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Input, Flatten
+from keras.layers import GlobalMaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import Concatenate
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
 
-def lenet(input_shape, comp=False, summary=False):
-	model = Sequential()
-
-	model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',input_shape=input_shape))
-	model.add(Conv2D(64, (3, 3), activation='relu'))
-	
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	model.add(Dropout(0.25))
-	
-	model.add(Flatten())
-	
-	model.add(Dense(128, activation='relu'))
-	model.add(Dropout(0.5))
-
-	model.add(Dense(265, activation='relu'))
-	model.add(Dropout(0.5))
-	
-	model.add(Dense(1, activation='logistic'))
-	
-	if summary:
-		model.summary()
-	if comp:
-		model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
-
-	return model
+def get_callbacks(filepath, patience=2):
+    es = EarlyStopping('val_loss', patience=patience, mode="min")
+    msave = ModelCheckpoint(filepath, save_best_only=True)
+    return [es, msave]
+    
+def get_model():
+    bn_model = 0
+    p_activation = "elu"
+    input_1 = Input(shape=(75, 75, 3), name="X_1")
+    input_2 = Input(shape=[1], name="angle")
+    
+    img_1 = Conv2D(16, kernel_size = (3,3), activation=p_activation) ((BatchNormalization(momentum=bn_model))(input_1))
+    img_1 = Conv2D(16, kernel_size = (3,3), activation=p_activation) (img_1)
+    img_1 = MaxPooling2D((2,2)) (img_1)
+    img_1 = Dropout(0.2)(img_1)
+    img_1 = Conv2D(32, kernel_size = (3,3), activation=p_activation) (img_1)
+    img_1 = Conv2D(32, kernel_size = (3,3), activation=p_activation) (img_1)
+    img_1 = MaxPooling2D((2,2)) (img_1)
+    img_1 = Dropout(0.2)(img_1)
+    img_1 = Conv2D(64, kernel_size = (3,3), activation=p_activation) (img_1)
+    img_1 = Conv2D(64, kernel_size = (3,3), activation=p_activation) (img_1)
+    img_1 = MaxPooling2D((2,2)) (img_1)
+    img_1 = Dropout(0.2)(img_1)
+    img_1 = Conv2D(128, kernel_size = (3,3), activation=p_activation) (img_1)
+    img_1 = MaxPooling2D((2,2)) (img_1)
+    img_1 = Dropout(0.2)(img_1)
+    img_1 = GlobalMaxPooling2D() (img_1)
+    
+    
+    img_2 = Conv2D(128, kernel_size = (3,3), activation=p_activation) ((BatchNormalization(momentum=bn_model))(input_1))
+    img_2 = MaxPooling2D((2,2)) (img_2)
+    img_2 = Dropout(0.2)(img_2)
+    img_2 = GlobalMaxPooling2D() (img_2)
+    
+    img_concat =  (Concatenate()([img_1, img_2, BatchNormalization(momentum=bn_model)(input_2)]))
+    
+    dense_ayer = Dropout(0.5) (BatchNormalization(momentum=bn_model) ( Dense(256, activation=p_activation)(img_concat) ))
+    dense_ayer = Dropout(0.5) (BatchNormalization(momentum=bn_model) ( Dense(64, activation=p_activation)(dense_ayer) ))
+    output = Dense(1, activation="sigmoid")(dense_ayer)
+    
+    model = Model([input_1,input_2],  output)
+    optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+    return model
--- train.py
+++ train.py
@@ -1,50 +1,104 @@
-import os
 import models
-import utils
 
+import numpy as np
 import pandas as pd
-import numpy as np 
 
+from sklearn.model_selection import train_test_split
 from subprocess import check_output
+from matplotlib import pyplot
 
-###### TRAINING CONSTANTS ######
-SPLIT = 0.2
-BATCH_SIZE = 64
-EPOCHS = 25
-VAL_SAMPLES = 30000
-SAMPLES_PER_EPOCH = (20000//BATCH_SIZE)*BATCH_SIZE
-############################
+from keras.preprocessing.image import ImageDataGenerator
 
-# Check Files Availability
 print(check_output(["ls", "./dataset/"]).decode("utf8"))
 
-# Reading the dataset (Training/Testing)
-trainjson = pd.read_json('./dataset/train.json')
-testjson = pd.read_json('./dataset/test.json')
+#Load data
+train = pd.read_json("./dataset/train.json")
+test = pd.read_json("./dataset/test.json")
+train.inc_angle = train.inc_angle.replace('na', 0)
+train.inc_angle = train.inc_angle.astype(float).fillna(0.0)
+print("loading files done!")
 
-# Splitting the training and validation data
-train_num = (trainjson.shape[0]*SPLIT)
-training_data = trainjson.loc[0:train_num-1]
-validation_data = trainjson.loc[train_num:]
+# Process the data and splitting
+x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train["band_1"]])
+x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train["band_2"]])
+X_train = np.concatenate([x_band1[:, :, :, np.newaxis], 
+                         x_band2[:, :, :, np.newaxis], 
+                        ((x_band1+x_band1)/2)[:, :, :, np.newaxis]], axis=-1)
+X_angle_train = np.array(train.inc_angle)
+y_train = np.array(train["is_iceberg"])
 
-# Freeing the memory from that big shit
-trainjson = None
+# Test data
+x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test["band_1"]])
+x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test["band_2"]])
+X_test = np.concatenate([x_band1[:, :, :, np.newaxis], 
+                        x_band2[:, :, :, np.newaxis], 
+                        ((x_band1+x_band1)/2)[:, :, :, np.newaxis]], axis=-1)
+X_angle_test = np.array(test.inc_angle)
 
-# # Generate training data and validation data
-# train = utils.gen_batch(training_data, BATCH_SIZE)
-# valid = utils.gen_batch(validation_data, BATCH_SIZE)
+X_train, X_valid, X_angle_train, X_angle_valid, y_train, y_valid = train_test_split(X_train
+                    , X_angle_train, y_train, random_state=123, train_size=0.75)
 
-# model = models.lenet(input_shape=(64,64,1), comp=True, summary=True)
+print("X_train: ", X_train.shape)
+print("X_angle_train: ", X_angle_train.shape)
+print("y_train: ", y_train.shape)
 
-# history = model.fit_generator(train, samples_per_epoch=SAMPLES_PER_EPOCH,
-#                             nb_epoch=EPOCHS, validation_data=valid,
-#                             nb_val_samples=VAL_SAMPLES)
+print("***************")
 
-# json = model.to_json()
-# model.save_weights('../../save/model.h5')
-# with open('../../save/model.json', 'w') as f:
-#     f.write(json)
+print("X_valid: ", X_valid.shape)
+print("X_angle_valid: ", X_angle_valid.shape)
+print("y_valid: ", y_valid.shape)
+
+print("***************")
+
+print("X_test: ", X_test.shape)
+print("X_angle_test: ", X_angle_test.shape)
+
+model = models.get_model()
+model.summary()
+
+gen = ImageDataGenerator(horizontal_flip = True,
+                         vertical_flip = True,
+                         width_shift_range = 0.1,
+                         height_shift_range = 0.1,
+                         zoom_range = 0.1,
+                         rotation_range = 40)
+
+def gen_flow_for_two_inputs(X1, X2, y):
+    genX1 = gen.flow(X1,y,  batch_size=batch_size,seed=666)
+    genX2 = gen.flow(X1,X2, batch_size=batch_size,seed=666)
+    while True:
+            X1i = genX1.next()
+            X2i = genX2.next()
+            #Assert arrays are equal - this was for peace of mind, but slows down training
+            #np.testing.assert_array_equal(X1i[0],X2i[0])
+            yield [X1i[0], X2i[1]], X1i[1]
+            
+gen_flow = gen_flow_for_two_inputs(X_train, X_angle_train, y_train)
 
 
+file_path = "model_weights.hdf5"
+callbacks = models.get_callbacks(filepath=file_path, patience=5)
+
+model.fit_generator(gen_flow
+                    ,validation_data=([X_valid, X_angle_valid], y_valid)
+                     ,steps_per_epoch=len(X_train)/32
+                    ,epochs = 25
+                     ,callbacks=modelcallbacks)
 
 
+model.load_weights(filepath=file_path)
+
+print("Train evaluate:")
+print(model.evaluate([X_train, X_angle_train], y_train, verbose=1, batch_size=200))
+
+print("####################")
+
+print("watch list evaluate:")
+print(model.evaluate([X_valid, X_angle_valid], y_valid, verbose=1, batch_size=200))
+
+prediction = model.predict([X_test, X_angle_test], verbose=1, batch_size=200)
+
+submission = pd.DataFrame({'id': test["id"], 'is_iceberg': prediction.reshape((prediction.shape[0]))})
+submission.head(10)
+
+submission.to_csv("./submission.csv", index=False)
